d4f9a536c7cf12219da3864f8be3c59d394fef3a
Karken Turarov <karken_turarov@mail.ru>
2023-10-25 00:59:42 +0600

    add SumFunction.test.ts

--- __tests__/SumFunction.test.ts
+++ __tests__/SumFunction.test.ts
@@ -1,20 +1,20 @@
-import { sum } from '../src/SumFunction';
+import { sum } from "../src/SumFunction";
 
-describe('sum function', () => {
-  it('should return 0 when called without arguments', () => {
-    expect(sum().toString()).toBe('0');
+describe("sum function", () => {
+  it("should return 0 when called without arguments", () => {
+    expect(sum().toString()).toBe("0");
   });
 
-  it('should return correct sum when called with multiple chained calls', () => {
-    expect(sum(1)(2).toString()).toBe('3');
-    expect(sum(3)(4)(5).toString()).toBe('12');
+  it("should return correct sum when called with multiple chained calls", () => {
+    expect(sum(1)(2).toString()).toBe("3");
+    expect(sum(3)(4)(5).toString()).toBe("12");
   });
 
-  it('should work with stored references', () => {
+  it("should work with stored references", () => {
     const s = sum();
-    expect(s(1).toString()).toBe('1');
+    expect(s(1).toString()).toBe("1");
     const s3 = sum(3);
-    expect(s3(5).toString()).toBe('8');
-    expect(s3(6).toString()).toBe('9');
+    expect(s3(5).toString()).toBe("8");
+    expect(s3(6).toString()).toBe("9");
   });
 });
